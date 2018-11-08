a9ee6e380f07d65c3a20358e36a2508075102e01
Petros Stergioulas <petros.stergioulas94@gmail.com>
2018-11-08 20:43:52 +0100

    controller

---
+++ src/main/kotlin/com/stergioulas/example/springboot/kotlin/TodoItemController.kt
@@ -0,0 +1,12 @@
+package com.stergioulas.example.springboot.kotlin
+
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+
+@RestController
+@RequestMapping("/api/todoItems")
+class TodoItemController {
+
+    // TODO("should implement routes")
+}
