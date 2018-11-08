964d0c008a4886436fa388e764f6194600e533b5
Petros Stergioulas <petros.stergioulas94@gmail.com>
2018-11-08 20:28:46 +0100

    init

---
+++ src/main/kotlin/com/stergioulas/example/springboot/kotlin/Main.kt
@@ -0,0 +1,11 @@
+package com.stergioulas.example.springboot.kotlin
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Anywayproject2Application
+
+fun main(args: Array<String>) {
+    runApplication<Anywayproject2Application>(*args)
+}
