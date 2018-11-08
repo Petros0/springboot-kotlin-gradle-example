e4f31e352ad908d4f60077d3b057535aa5ed71f2
Petros Stergioulas <petros.stergioulas94@gmail.com>
2018-11-08 20:34:29 +0100

    rename

--- src/test/kotlin/com/stergioulas/example/springboot/kotlin/MainApplicationTests.kt
+++ src/test/kotlin/com/stergioulas/example/springboot/kotlin/MainApplicationTests.kt
@@ -11,7 +11,7 @@ import org.springframework.test.context.junit4.SpringRunner
 
 @RunWith(SpringRunner::class)
 @SpringBootTest
-class Anywayproject2ApplicationTests {
+class MainApplicationTests {
 
 
     @Autowired
